4e03ff6546ffbd2cfa2ebe2d3aa148ad8fd87d43
c <hackerjieson@gmail.com>
2020-05-08 20:48:39 +0800

    modify

--- expression.go
+++ expression.go
@@ -7,79 +7,79 @@ import (
 )
 
 //表达式
-type Expression struct {
-	Minute         []int  //分 规则
-	Hour           []int  //时 规则
-	Dom            []int  //日 规则
-	DomCopy        []int  //日 规则拷贝
-	Month          []int  //月 规则
-	Dow            []int  //周 规则
-	LocationOffset int    //当前时区与UTC的偏移秒数
-	LocationName   string //当前时区名
-	CronRule       string //原始的cron规则
-	IsParse        bool   //是否解析
+type expression struct {
+	minute         []int  //分 规则
+	hour           []int  //时 规则
+	dom            []int  //日 规则
+	domCopy        []int  //日 规则拷贝
+	month          []int  //月 规则
+	dow            []int  //周 规则
+	locationOffset int    //当前时区与UTC的偏移秒数
+	locationName   string //当前时区名
+	cronRule       string //原始的cron规则
+	isParse        bool   //是否解析
 }
 
 //规则键位
 const (
-	MinuteKey = iota
-	HourKey
-	DomKey
-	MonthKey
-	DowKey
+	minuteKey = iota
+	hourKey
+	domKey
+	monthKey
+	dowKey
 )
 
 //创建一个表达式 参数实例: "* 1-10/2,1,2,3 * */2 *", "CST", 8*3600
-func NewExpression(cronExpr, locationName string, locationOffset int) *Expression {
-	return &Expression{
-		Minute:         make([]int, 0),
-		Hour:           make([]int, 0),
-		Dom:            make([]int, 0),
-		DomCopy:        make([]int, 0),
-		Month:          make([]int, 0),
-		Dow:            make([]int, 0),
-		LocationOffset: locationOffset,
-		LocationName:   locationName,
-		CronRule:       cronExpr,
-		IsParse:        false,
+func NewExpression(cronExpr, locationName string, locationOffset int) *expression {
+	return &expression{
+		minute:         make([]int, 0),
+		hour:           make([]int, 0),
+		dom:            make([]int, 0),
+		domCopy:        make([]int, 0),
+		month:          make([]int, 0),
+		dow:            make([]int, 0),
+		locationOffset: locationOffset,
+		locationName:   locationName,
+		cronRule:       cronExpr,
+		isParse:        false,
 	}
 }
 
 //解析
-func (expr *Expression) Parse() error {
-	if strings.ContainsRune(expr.CronRule, '@') {
-		v, ok := alias[expr.CronRule]
+func (expr *expression) parse() error {
+	if strings.ContainsRune(expr.cronRule, '@') {
+		v, ok := alias[expr.cronRule]
 		if !ok {
 			return errors.New("不支持的宏")
 		}
-		expr.CronRule = v
+		expr.cronRule = v
 	}
-	ruleItems := strings.Split(expr.CronRule, " ")
+	ruleItems := strings.Split(expr.cronRule, " ")
 	if len(ruleItems) != 5 {
 		return errors.New("cron规则格式错误")
 	}
 	var err error
-	expr.Minute, err = cronRuleParse(ruleItems[MinuteKey], []int{0, 59})
+	expr.minute, err = cronRuleParse(ruleItems[minuteKey], []int{0, 59})
 	if err != nil {
 		return errors.New("分钟" + err.Error())
 	}
-	expr.Hour, err = cronRuleParse(ruleItems[HourKey], []int{0, 23})
+	expr.hour, err = cronRuleParse(ruleItems[hourKey], []int{0, 23})
 	if err != nil {
 		return errors.New("小时" + err.Error())
 	}
-	expr.Dom, err = cronRuleParse(ruleItems[DomKey], []int{1, 31})
+	expr.dom, err = cronRuleParse(ruleItems[domKey], []int{1, 31})
 	if err != nil {
 		return errors.New("日(月)" + err.Error())
 	}
-	expr.DomCopy = make([]int, len(expr.Dom))
-	_ = copy(expr.DomCopy, expr.Dom)
-	monthExpr := monthAliasToNumber(ruleItems[MonthKey])
-	expr.Month, err = cronRuleParse(monthExpr, []int{1, 12})
+	expr.domCopy = make([]int, len(expr.dom))
+	_ = copy(expr.domCopy, expr.dom)
+	monthExpr := monthAliasToNumber(ruleItems[monthKey])
+	expr.month, err = cronRuleParse(monthExpr, []int{1, 12})
 	if err != nil {
 		return errors.New("月" + err.Error())
 	}
-	dowExpr := DowAliasToNumber(ruleItems[DowKey])
-	expr.Dow, err = cronRuleParse(dowExpr, []int{0, 6})
+	dowExpr := DowAliasToNumber(ruleItems[dowKey])
+	expr.dow, err = cronRuleParse(dowExpr, []int{0, 6})
 	if err != nil {
 		return errors.New("周(月)" + err.Error())
 	}
@@ -104,7 +104,7 @@ type nextAt struct {
 	week   int
 }
 
-func (expr *Expression) nextMonth(now time.Time, change *change, nextAt *nextAt, jump *string) bool {
+func (expr *expression) nextMonth(now time.Time, change *change, nextAt *nextAt, jump *string) bool {
 	change.month = 0
 	change.day = 0
 	change.hour = 0
@@ -113,34 +113,34 @@ func (expr *Expression) nextMonth(now time.Time, change *change, nextAt *nextAt,
 	current := int(now.Month())
 	if *jump == "month" {
 		change.month = 1
-		nextAt.month = getMinValueInArray(expr.Month, current)
+		nextAt.month = getMinValueInArray(expr.month, current)
 		if nextAt.month <= current {
 			nextAt.year += 1
 		}
 	} else {
 		nextAt.month = current
-		if !existsInArray(expr.Month, current) {
-			nextAt.month = getMinValueInArray(expr.Month, current)
+		if !existsInArray(expr.month, current) {
+			nextAt.month = getMinValueInArray(expr.month, current)
 			change.month = 1
 		}
 	}
-	expr.Dom = make([]int, len(expr.DomCopy))
-	_ = copy(expr.Dom, expr.DomCopy)
+	expr.dom = make([]int, len(expr.domCopy))
+	_ = copy(expr.dom, expr.domCopy)
 	expr.weekToDay(now, change, nextAt)
 	return true
 }
-func (expr *Expression) nextDay(now time.Time, change *change, nextAt *nextAt, jump *string) bool {
+func (expr *expression) nextDay(now time.Time, change *change, nextAt *nextAt, jump *string) bool {
 	current := now.Day()
 	if *jump != "day" {
 		if change.month == 1 {
-			nextAt.day = expr.Dom[0]
+			nextAt.day = expr.dom[0]
 			if nextAt.day != current {
 				change.day = 1
 			}
 			return true
 		}
-		if !existsInArray(expr.Dom, current) {
-			nextAt.day = getMinValueInArray(expr.Dom, current)
+		if !existsInArray(expr.dom, current) {
+			nextAt.day = getMinValueInArray(expr.dom, current)
 			if nextAt.day < current {
 				*jump = "month"
 				return false
@@ -152,7 +152,7 @@ func (expr *Expression) nextDay(now time.Time, change *change, nextAt *nextAt, j
 			nextAt.day = current
 		}
 	} else {
-		nextAt.day = getMinValueInArray(expr.Dom, current)
+		nextAt.day = getMinValueInArray(expr.dom, current)
 		if nextAt.day <= current {
 			*jump = "month"
 			return false
@@ -161,18 +161,18 @@ func (expr *Expression) nextDay(now time.Time, change *change, nextAt *nextAt, j
 	}
 	return true
 }
-func (expr *Expression) nextHour(now time.Time, change *change, nextAt *nextAt, jump *string) bool {
+func (expr *expression) nextHour(now time.Time, change *change, nextAt *nextAt, jump *string) bool {
 	current := now.Hour()
 	if *jump != "hour" {
 		if change.day == 1 || change.month == 1 {
-			nextAt.hour = expr.Hour[0]
+			nextAt.hour = expr.hour[0]
 			if nextAt.hour != current {
 				change.hour = 1
 			}
 			return true
 		}
-		if !existsInArray(expr.Hour, current) {
-			nextAt.hour = getMinValueInArray(expr.Hour, current)
+		if !existsInArray(expr.hour, current) {
+			nextAt.hour = getMinValueInArray(expr.hour, current)
 			if nextAt.hour < current {
 				*jump = "day"
 				return false
@@ -184,7 +184,7 @@ func (expr *Expression) nextHour(now time.Time, change *change, nextAt *nextAt, 
 			nextAt.hour = current
 		}
 	} else {
-		nextAt.hour = getMinValueInArray(expr.Hour, current)
+		nextAt.hour = getMinValueInArray(expr.hour, current)
 		if nextAt.hour <= current {
 			*jump = "day"
 			return false
@@ -193,12 +193,12 @@ func (expr *Expression) nextHour(now time.Time, change *change, nextAt *nextAt, 
 	}
 	return true
 }
-func (expr *Expression) nextMinute(now time.Time, change *change, nextAt *nextAt, jump *string) bool {
+func (expr *expression) nextMinute(now time.Time, change *change, nextAt *nextAt, jump *string) bool {
 	current := now.Minute()
 	if change.hour == 1 || change.day == 1 || change.month == 1 {
-		nextAt.minute = expr.Minute[0]
+		nextAt.minute = expr.minute[0]
 	} else {
-		nextAt.minute = getMinValueInArray(expr.Minute, current)
+		nextAt.minute = getMinValueInArray(expr.minute, current)
 		if nextAt.minute <= current {
 			*jump = "hour"
 			return false
@@ -208,21 +208,21 @@ func (expr *Expression) nextMinute(now time.Time, change *change, nextAt *nextAt
 }
 
 //dow转dom
-func (expr *Expression) weekToDay(now time.Time, change *change, nextAt *nextAt) {
+func (expr *expression) weekToDay(now time.Time, change *change, nextAt *nextAt) {
 
-	ruleItems := strings.Split(expr.CronRule, " ")
-	if ruleItems[DowKey] == "*" {
+	ruleItems := strings.Split(expr.cronRule, " ")
+	if ruleItems[dowKey] == "*" {
 		return
 	}
 	//dom和dow任意一个存在 间隔符 / 将形成交集
-	days := getDayByWeek(now.Year(), nextAt.month, expr.Dow, expr.LocationName, expr.LocationOffset)
-	if strings.Contains(ruleItems[DowKey], "*/") || strings.Contains(ruleItems[DomKey], "*/") {
-		expr.Dom = arrayIntersect(expr.Dom, days)
+	days := getDayByWeek(now.Year(), nextAt.month, expr.dow, expr.locationName, expr.locationOffset)
+	if strings.Contains(ruleItems[dowKey], "*/") || strings.Contains(ruleItems[domKey], "*/") {
+		expr.dom = arrayIntersect(expr.dom, days)
 	} else {
-		if ruleItems[DomKey] != "*" {
-			expr.Dom = arrayMerge(expr.Dom, days)
+		if ruleItems[domKey] != "*" {
+			expr.dom = arrayMerge(expr.dom, days)
 		} else {
-			expr.Dom = days
+			expr.dom = days
 		}
 
 	}
@@ -231,26 +231,26 @@ func (expr *Expression) weekToDay(now time.Time, change *change, nextAt *nextAt)
 //计算下一个执行日期
 //开始时间,下几个执行点 接收结果
 //tip:指定不同的开始时间可以实现时间穿梭
-func (expr *Expression) Next(current time.Time, nextStep int, dst *[]string) error {
-	if expr.IsParse == false {
-		err := expr.Parse()
+func (expr *expression) Next(current time.Time, nextStep int, dst *[]string) error {
+	if expr.isParse == false {
+		err := expr.parse()
 		if err != nil {
 			return err
 		}
 	}
-	now := current.In(time.FixedZone(expr.LocationName, expr.LocationOffset))
+	now := current.In(time.FixedZone(expr.locationName, expr.locationOffset))
 	var (
 		change = &change{}
 		nextAt = &nextAt{}
 	)
 	//当前周期的上一个周期是否需要跳跃
 	var jump = ""
-	expr.Recursive(now, change, nextAt, &jump)
-	//fmt.Println("分",expr.Minute)
-	//fmt.Println("时",expr.Hour)
-	//fmt.Println("日",expr.Dom)
-	//fmt.Println("月",expr.Month)
-	//fmt.Println("周",expr.Dow)
+	expr.recursive(now, change, nextAt, &jump)
+	//fmt.Println("分",expr.minute)
+	//fmt.Println("时",expr.hour)
+	//fmt.Println("日",expr.dom)
+	//fmt.Println("月",expr.month)
+	//fmt.Println("周",expr.dow)
 	res := time.Date(nextAt.year, time.Month(nextAt.month), nextAt.day, nextAt.hour, nextAt.minute, 0, 0, now.Location())
 	*dst = append(*dst, res.Format("2006-01-02 15:04:05"))
 	if nextStep == 1 {
@@ -260,15 +260,15 @@ func (expr *Expression) Next(current time.Time, nextStep int, dst *[]string) err
 }
 
 //计算下一个日期
-func (expr *Expression) Recursive(current time.Time, change *change, at *nextAt, jump *string) {
+func (expr *expression) recursive(current time.Time, change *change, at *nextAt, jump *string) {
 	expr.nextMonth(current, change, at, jump)
 	if b := expr.nextDay(current, change, at, jump); !b {
-		expr.Recursive(current, change, at, jump)
+		expr.recursive(current, change, at, jump)
 	}
 	if b := expr.nextHour(current, change, at, jump); !b {
-		expr.Recursive(current, change, at, jump)
+		expr.recursive(current, change, at, jump)
 	}
 	if b := expr.nextMinute(current, change, at, jump); !b {
-		expr.Recursive(current, change, at, jump)
+		expr.recursive(current, change, at, jump)
 	}
 }
